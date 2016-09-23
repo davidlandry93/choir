994040097073875fe338cbf19fa9fd3a13e5bb00
David Landry <davidlandry93@gmail.com>
2016-09-23 15:07:53 -0400

    Wrapped the thing in a class

--- choir.py
+++ choir.py
@@ -2,70 +2,85 @@
 import time
 import serial
 
-codes_of_notes = {'C': (0x05, 0x6E),
-                'D': (0x04, 0xD6),
-                'E': (0x04, 0x4F),
-                'F': (0x04, 0x11),
-                'G': (0x03, 0xA0),
-                'A': (0x03, 0x3A),
-                'B': (0x02, 0xE0)}
 
-def checksum(bytearray):
-    checksum = 0x00
-    for byte in bytearray:
-        checksum = checksum ^ byte
+class Kobuki:
+    TUNING_CONSTANT = 0.00000275
 
-    print(checksum)
-    return checksum
+    frequencies_of_notes = {
+        48: 261.63, # C4
+        49: 277.18,
+        50: 293.66, # D4
+        51: 311.13,
+        52: 329.63, # E4
+        53: 349.23, # F4
+        54: 369.99,
+        55: 392.0,  # G4
+        56: 415.30,
+        57: 440.0, # A4
+        58: 466.16,
+        59: 493.88, # B4
+        60: 523.25, # C5
+    }
 
-def wrap_payload(payload):
-    wrapped_payload = payload.copy()
-    wrapped_payload.insert(0, len(payload))
-    wrapped_payload.append(checksum(payload))
+    def __init__(self):
+        pass
 
-    print(wrapped_payload)
+    def __enter__(self):
+        self.comm  = serial.Serial(
+            port='/dev/kobuki',
+            baudrate=115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS)
+        return self
 
-    wrapped_payload.insert(0, 0x55)
-    wrapped_payload.insert(0, 0xAA)
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.comm.close()
 
-    return wrapped_payload
+    def bytes_of_note(self, note):
+        kobuki_encoded_note = int(round(1 / (self.frequencies_of_notes[note] *
+                                             self.TUNING_CONSTANT)))
+        print(kobuki_encoded_note)
 
-def payload_of_note(note, duration):
-    payload = bytearray([0x03, 0x03])
+        return kobuki_encoded_note.to_bytes(2, byteorder='little')
 
-    code_of_note = codes_of_notes[note]
-    payload.append(code_of_note[1]) # less significant byte first
-    payload.append(code_of_note[0])
-    payload.append(duration)
+    def checksum(self, bytearray):
+        checksum = 0x00
+        for byte in bytearray:
+            checksum = checksum ^ byte
 
-    return payload
+        print(checksum)
+        return checksum
 
-kobuki = serial.Serial(
-    port='/dev/kobuki',
-    baudrate=115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-)
+    def wrap_payload(self, payload):
+        wrapped_payload = payload.copy()
+        wrapped_payload.insert(0, len(payload))
+        wrapped_payload.append(self.checksum(payload))
 
-print(kobuki.is_open)
+        print(wrapped_payload)
 
-# First byte: play a sound
-# Second byte: the size of the payload (1)
-# third byte: play the happy robot sound
-#payload = bytearray([0x04, 0x01, 0x01])
-#kobuki.write(wrap_payload(payload))
+        wrapped_payload.insert(0, 0x55)
+        wrapped_payload.insert(0, 0xAA)
 
-# First byte: play an arbitrary sound
-# Second byte: the size of the payload (3)
-# third and fourth bytes: the note to play
-# fifth byte: the duration in milis
+        return wrapped_payload
+
+    def payload_of_note(self, note, duration):
+        payload = bytearray([0x03, 0x03])
+
+        code_of_note = self.bytes_of_note(note)
+        print(code_of_note)
+        payload.append(code_of_note[0])
+        payload.append(code_of_note[1])
+        payload.append(duration)
+
+        return payload
+
+    def play_note(self, note):
+        payload = self.wrap_payload(self.payload_of_note(note, 20))
+        self.comm.write(payload)
+        time.sleep(0.012)
 
 
-for _ in range(1000):
-    payload = wrap_payload(payload_of_note('B', 255))
-    kobuki.write(payload)
-    time.sleep(0.05)
-
-
-kobuki.close()
+with Kobuki() as kobuki:
+    while True:
+        kobuki.play_note(49)
