e019ea5ea6644dfafafc249136e4b8a556e9e2a8
David Landry <davidlandry93@gmail.com>
2016-09-26 18:31:32 -0400

    ADded blinkenlights

--- start_all_servers.sh
+++ start_all_servers.sh
@@ -3,7 +3,7 @@
 for ip in 192.168.0.{101,102,104,105,106,109,111,114}
 do
     echo "Starting ${ip}"
-    ssh -t robmob@${ip} "python3 /home/robmob/choir/singer_server.py &"
+    ssh robmob@${ip} "nohup python3 /home/robmob/choir/singer_server.py &"
 done
 
 echo "Done."
